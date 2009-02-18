d2d5485e6af0a36160ac1b468d56dd0c3197af1b
Hervé Cauwelier <herve@itaapy.com>
2009-02-18 14:18:46 +0100

    python: skeleton of the test suite

---
+++ python/test/test.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+# Copyright (C) 2009 Itaapy, ArsAperta, Pierlis, Talend
+
+# Import from the Standard Library
+from unittest import TestCase, TestLoader, TestSuite, TextTestRunner
+
+# Import tests
+
+
+test_modules = []
+
+
+loader = TestLoader()
+
+if __name__ == '__main__':
+    suite = TestSuite()
+    for module in test_modules:
+        suite.addTest(loader.loadTestsFromModule(module))
+
+    TextTestRunner(verbosity=1).run(suite)
