398503e596bb6adea2c5f92e4d480491da51f77e
Hervé Cauwelier <herve@itaapy.com>
2009-02-26 19:37:51 +0100

    python: use the libxml2 wrapper as a beginning

--- python/context.py
+++ python/context.py
@@ -1,13 +1,8 @@
 # -*- coding: UTF-8 -*-
 # Copyright (C) 2009 Itaapy, ArsAperta, Pierlis, Talend
 
-# Import from itools
-from itools.xml import XMLParser, stream_to_str
-
-
-
-def create_element(data):
-    return XMLParser(data)
+# Import from libxml2
+from libxml2 import parseMemory
 
 
 
@@ -16,24 +11,45 @@ class odf_element(object):
     Abstraction of the XML library behind.
     """
 
-    def get_element_list(self, xpath):
-        raise NotImplementedError
+    def __init__(self, internal_element):
+        # TODO check is element node
+        self.__element = internal_element
+
+
+    def get_element_list(self, xpath_expression):
+        element = self.__element
+        xpath_context = element.xpathNewContext()
+        result = xpath_context.xpathEval(xpath_expression)
+        xpath_context.xpathFreeContext()
+        # TODO only element nodes
+        return [odf_element(e) for e in result]
 
 
     def get_attribute(self, name):
-        raise NotImplementedError
+        element = self.__element
+        properties = element.properties
+        if properties is None:
+            return None
+        return properties.get(name)
 
 
-    def set_attribute(self, name):
-        raise NotImplementedError
+    def set_attribute(self, name, value):
+        element = self.__element
+        element.setProp(name, value)
 
     
     def get_text(self):
-        raise NotImplementedError
+        # XXX all text recursively is need?
+        element = self.__element
+        # FIXME encoding?
+        return element.getContent()
 
 
     def set_text(self, text):
-        raise NotImplementedError
+        element = self.__element
+        # TODO apply the warning in help(elt.setContent)
+        # FIXME encoding?
+        element.setContent(text)
 
 
     def insert_element(self, element, position):
@@ -41,7 +57,9 @@ class odf_element(object):
 
 
     def copy(self):
-        raise NotImplementedError
+        element = self.__element
+        # TODO only element nodes
+        return [odf_element(e) for e in element.copyNodeList()]
 
 
     def delete(self):
@@ -59,20 +77,39 @@ class odf_context(object):
         self.container = container
 
         # Internal state
-        self.__events = None
+        self.__document = None
 
 
-    def __get_events(self):
-        if self.__events is None:
+    def __del__(self):
+        if self.__document is not None:
+            self.__document.freeDoc()
+
+
+    def __get_document(self):
+        if self.__document is None:
             container = self.container
             part = container.get_part(self.part_name)
-            self.__events = XMLParser(part)
-        return self.__events
+            self.__document = parseMemory(part, len(part))
+        return self.__document
 
 
-    def get_element_list(self, xpath):
-        raise NotImplementedError
+    def get_element_list(self, xpath_expression):
+        document = self.__get_document()
+        xpath_context = document.xpathNewContext()
+        result = xpath_context.xpathEval(xpath_expression)
+        xpath_context.xpathFreeContext()
+        # TODO only element nodes
+        return [odf_element(e) for e in result]
 
 
     def serialize(self):
-        return stream_to_str(self.__get_events())
+        # TODO another method to write back in the container?
+        return str(self.__get_document())
+
+
+
+def create_element(data):
+    document = parseMemory(data, len(data))
+    copy = document.children.copyNodeList()
+    document.freeDoc()
+    return odf_element(copy)
