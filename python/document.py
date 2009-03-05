bb112ed07091aeee73c7f45dd9221b8342773821
Hervé Cauwelier <herve@itaapy.com>
2009-03-05 16:38:17 +0100

    python: asking the xml part for the xpath results

--- python/document.py
+++ python/document.py
@@ -57,25 +57,39 @@ class odf_document(object):
             raise TypeError, "container is not an ODF container"
         self.container = container
 
+        # Cache of XML parts
+        self.__content = None
+        self.__styles = None
+        self.__meta = None
+
+
+    def __get_xmlpart(self, part_name):
+        part = getattr(self, '__' + part_name, None)
+        if part is None:
+            container = self.container
+            part = odf_xmlpart(part_name, container)
+            setattr(self, '__' + part_name, part)
+        return part
+
     #
     # Paragraphs
     #
 
     def get_paragraph_list(self, style=None, context=None):
         check_arguments(style=style, context=context)
-        container = self.container
+        content = self.__get_xmlpart('content')
         attributes = {}
         if style:
             attributes['text:style-name'] = style
         query = generate_xpath_query('text:p', attributes)
-        return container.get_element_list(query, context=context)
+        return content.get_element_list(query, context=context)
 
 
     def get_paragraph(self, position, context=None):
         check_arguments(position=position, context=context)
-        container = self.container
+        content = self.__get_xmlpart('content')
         query = generate_xpath_query('text:p', position)
-        return container.get_element_list(query, context=context)
+        return content.get_element_list(query, context=context)
 
 
     def insert_paragraph(self, element, context=None):
@@ -88,21 +102,21 @@ class odf_document(object):
 
     def get_heading_list(self, style=None, level=None, context=None):
         check_arguments(style=style, level=level, context=context)
-        container = self.container
+        content = self.__get_xmlpart('content')
         attributes = {}
         if style:
             attributes['text:style-name'] = style
         if level:
             attributes['text:level'] = level
         query = generate_xpath_query('text:h', attributes)
-        return container.get_element_list(query, context=context)
+        return content.get_element_list(query, context=context)
 
 
     def get_heading(self, position, level=None, context=None):
         check_arguments(position=position, level=level, context=context)
-        container = self.container
+        content = self.__get_xmlpart('content')
         query = generate_xpath_query('text:h', position)
-        return container.get_element_list(query, context=context)
+        return content.get_element_list(query, context=context)
 
 
     def insert_heading(self, element, context=None):
