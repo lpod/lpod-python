2989ed71cc4c06283dd65e9a4c0582c4f26f5b9a
Hervé Cauwelier <herve@itaapy.com>
2009-06-29 18:49:19 +0200

    document odf_document functions

--- python/document.py
+++ python/document.py
@@ -20,6 +20,13 @@ from xmlpart import odf_create_element
 #
 
 def odf_create_section(style):
+    """Create a section element of the given style.
+    Arguments:
+
+        style -- str
+
+    Return: odf_element
+    """
     _check_arguments(style=style)
     data = '<text:section text:style-name="%s"></text:section>' % style
     return odf_create_element(data)
@@ -27,6 +34,15 @@ def odf_create_section(style):
 
 
 def odf_create_paragraph(style, text=u''):
+    """Create a paragraph element of the given style containing the optional
+    given text.
+    Arguments:
+
+        style -- str
+        text -- unicode
+
+    Return: odf_element
+    """
     _check_arguments(style=style, text=text)
     data = '<text:p text:style-name="%s">%s</text:p>'
     text = text.encode('utf_8')
@@ -35,6 +51,18 @@ def odf_create_paragraph(style, text=u''):
 
 
 def odf_create_heading(style, level, text=u''):
+    """Create a heading element of the given style and level, containing the
+    optional given text.
+    Arguments:
+
+        style -- str
+        level -- int
+        text -- unicode
+
+    Return: odf_element
+
+    Level count begins at 1.
+    """
     _check_arguments(style=style, level=level, text=text)
     data = '<text:h text:style-name="%s" text:outline-level="%d">%s</text:h>'
     text = text.encode('utf_8')
@@ -43,6 +71,22 @@ def odf_create_heading(style, level, text=u''):
 
 
 def odf_create_frame(name, style, width, height, page=None, x=None, y=None):
+    """Create a frame element of the given style, width and height,
+    optionally positionned at the given x and y coordinates, in the given
+    page.
+    Arguments:
+
+        style -- str
+        width -- str
+        height -- str
+        page -- int
+        x -- str
+        y -- str
+
+    Return: odf_element
+
+    Width, height, x and y are strings including the units, e.g. "10cm".
+    """
     if page is None:
         anchor = 'text:anchor-type="paragraph"'
     else:
@@ -57,11 +101,29 @@ def odf_create_frame(name, style, width, height, page=None, x=None, y=None):
     return odf_create_element(data % (name, style, width, height, anchor))
 
 
-def odf_create_image(link):
-    return odf_create_element('<draw:image xlink:href="%s"/>' % link)
+def odf_create_image(uri):
+    """Create an image element showing the image at the given URI.
+    Arguments:
+
+        uri -- str
+
+    Return: odf_element
+    """
+    return odf_create_element('<draw:image xlink:href="%s"/>' % uri)
 
 
 def odf_create_cell(cell_type='string', currency=None):
+    """Create a cell element of the optinnally given type. If cell_type is
+    "currency", the currency must be given.
+    Arguments:
+
+        cell_type -- 'boolean', 'currency', 'date', 'float', 'percentage',
+                     'string' or 'time'
+        currency -- three-letter code
+
+    Return: odf_element
+    """
+    # FIXME text content?
     _check_arguments(cell_type=cell_type, currency=currency)
     data = '<table:table-cell office:value-type="%s"/>'
     cell = odf_create_element(data % cell_type)
@@ -71,6 +133,13 @@ def odf_create_cell(cell_type='string', currency=None):
 
 
 def odf_create_row(width=None):
+    """Create a row element, optionally filled with "width" number of cells.
+    Arguments:
+
+        width -- int
+
+    Return: odf_element
+    """
     row = odf_create_element('<table:table-row/>')
     if width is not None:
         for i in xrange(width):
@@ -80,11 +149,28 @@ def odf_create_row(width=None):
 
 
 def odf_create_column(style):
+    """Create a column element of the given style.
+    Arguments:
+
+        style -- str
+
+    Return: odf_element
+    """
     data = '<table:table-column table:style-name="%s"/>'
     return odf_create_element(data % style)
 
 
 def odf_create_table(name, style, width=None, height=None):
+    """Create a table element of the given style, with "width" columns and
+    "height" rows.
+    Arguments:
+
+        style -- str
+        width -- int
+        height -- int
+
+    Return: odf_element
+    """
     data = '<table:table table:name="%s" table:style-name="%s"/>'
     table = odf_create_element(data % (name, style))
     if width is not None or height is not None:
@@ -97,18 +183,35 @@ def odf_create_table(name, style, width=None, height=None):
 
 
 def odf_create_item():
+    """Create a list item element.
+    Return: odf_element
+    """
+    # FIXME text content?
     return odf_create_element('<text:list-item/>')
 
 
 def odf_create_list(style):
+    """Create a list element of the given style.
+    Arguments:
+
+        style -- str
+
+    Return: odf_element
+    """
     return odf_create_element('<text:list text:style-name="%s"/>' % style)
 
 
 def odf_create_style(name, family='paragraph'):
-    """family can be:
-         paragraph, text, section, table, tablecolumn, table-row, table-cell,
-         table-page, chart, default, drawing-page, graphic, presentation,
-         control or ruby.
+    """Create a style element with the given name, related to the given
+    family.
+    Arguments:
+
+        name -- str
+        family -- 'paragraph', 'text', 'section', 'table', 'tablecolumn',
+                  'table-row', 'table-cell', 'table-page', 'chart',
+                  'default', 'drawing-page', 'graphic', 'presentation',
+                  'control' or 'ruby'
+    Return: odf_element
     """
     _check_arguments(family=family)
     data = '<style:style style:name="%s" style:family="%s"/>'
@@ -116,12 +219,23 @@ def odf_create_style(name, family='paragraph'):
 
 
 def odf_create_style_text_properties():
+    """Create a text properties element.
+    Return: odf_element
+    """
     # TODO should take parameters
     return odf_create_element('<style:text-properties/>')
 
 
 def odf_create_note(text, note_class='footnote', id=None):
-    """note_class = {footnote|endnote}
+    """Create either a footnote or a endnote element with the given text,
+    optionally referencing it using the given id.
+    Arguments:
+
+        text -- unicode
+        note_class -- 'footnote' or 'endnote'
+        id -- str
+    
+    Return: odf_element
     """
     _check_arguments(text=text, note_class=note_class)
     data = ('<text:note text:note-class="%s">'
@@ -138,6 +252,16 @@ def odf_create_note(text, note_class='footnote', id=None):
 
 
 def odf_create_annotation(creator, text, date=None):
+    """Create an annotation element credited to the given creator with the
+    given text, optionally dated (current date by default).
+    Arguments:
+
+        creator -- unicode
+        text -- unicode
+        date -- datetime
+    
+    Return: odf_element
+    """
     # TODO allow paragraph and text styles
     _check_arguments(creator=creator, text=text, date=date)
     data = ('<office:annotation>'
@@ -158,7 +282,8 @@ def odf_create_annotation(creator, text, date=None):
 #
 
 class odf_document(object):
-
+    """An abstraction of the Open Document file.
+    """
     def __init__(self, container):
         if not isinstance(container, odf_container):
             raise TypeError, "container is not an ODF container"
@@ -225,6 +350,9 @@ class odf_document(object):
 
 
     def clone(self):
+        """Return an exact copy of the document.
+        Return: odf_document
+        """
         clone = object.__new__(self.__class__)
         for name in self.__dict__:
             if name == 'container':
@@ -240,6 +368,15 @@ class odf_document(object):
 
 
     def save(self, uri=None, packaging=None, pretty=False):
+        """Save the document, at the same place it was opened or at the given
+        URI. It can be saved as a Zip file or as a plain XML file. The XML
+        can be pretty printed.
+        Arguments:
+
+            uri -- str
+            packaging -- 'zip' or 'flat'
+            pretty -- bool
+        """
         # Synchronize data with container
         for part_name, part in self.__xmlparts.items():
             if part is not None:
@@ -596,20 +733,27 @@ class odf_document(object):
 
 
     def get_language(self):
+        """Get the language code of the document, e.g. "fr-FR".
+        """
         meta = self.__get_xmlpart('meta')
         element = meta.get_element('//dc:language')
         return element.get_text()
 
 
     def set_language(self, language):
+        """Set the language code of the document, e.g. "fr-FR".
+        """
         if type(language) is not str:
             raise TypeError, 'language must be "xx-YY" ISO code'
+        # FIXME test validity?
         meta = self.__get_xmlpart('meta')
         element = meta.get_element('//dc:language')
         element.set_text(language)
 
 
     def get_modification_date(self):
+        """Get the last modified date of the document.
+        """
         meta = self.__get_xmlpart('meta')
         element = meta.get_element('//dc:date')
         date = element.get_text()
@@ -617,6 +761,8 @@ class odf_document(object):
 
 
     def set_modification_date(self, date):
+        """Set the last modified date of the document.
+        """
         _check_arguments(date=date)
         meta = self.__get_xmlpart('meta')
         element = meta.get_element('//dc:date')
@@ -625,6 +771,8 @@ class odf_document(object):
 
 
     def get_creation_date(self):
+        """Get the creation date of the document.
+        """
         meta = self.__get_xmlpart('meta')
         element = meta.get_element('//meta:creation-date')
         date = element.get_text()
@@ -632,6 +780,8 @@ class odf_document(object):
 
 
     def set_creation_date(self, date):
+        """Set the creation date of the document.
+        """
         _check_arguments(date=date)
         meta = self.__get_xmlpart('meta')
         element = meta.get_element('//meta:creation-date')
@@ -640,6 +790,9 @@ class odf_document(object):
 
 
     def get_keyword(self):
+        """Get the keyword(s) of the document.
+        Return: unicode
+        """
         meta = self.__get_xmlpart('meta')
         element = meta.get_element('//meta:keyword')
         keyword = element.get_text()
@@ -647,6 +800,10 @@ class odf_document(object):
 
 
     def set_keyword(self, keyword):
+        """Set the keyword(s) of the document.
+        Arguments:
+        keyword -- unicode
+        """
         _check_arguments(text=keyword)
         meta = self.__get_xmlpart('meta')
         element = meta.get_element('//meta:keyword')
@@ -655,6 +812,9 @@ class odf_document(object):
 
 
     def get_editing_duration(self):
+        """Get the time the document was edited, as reported by the generator.
+        Return: timedelta
+        """
         meta = self.__get_xmlpart('meta')
         element = meta.get_element('//meta:editing-duration')
         duration = element.get_text()
@@ -662,6 +822,10 @@ class odf_document(object):
 
 
     def set_editing_duration(self, duration):
+        """Set the time the document was edited.
+        Arguments:
+        duration -- timedelta
+        """
         if type(duration) is not timedelta:
             raise TypeError, u"duration must be a timedelta"
         meta = self.__get_xmlpart('meta')
@@ -671,6 +835,10 @@ class odf_document(object):
 
 
     def get_editing_cycles(self):
+        """Get the number of times the document was edited, as reported by the
+        generator.
+        Return: int
+        """
         meta = self.__get_xmlpart('meta')
         element = meta.get_element('//meta:editing-cycles')
         cycles = element.get_text()
@@ -678,6 +846,11 @@ class odf_document(object):
 
 
     def set_editing_cycles(self, cycles):
+        """Set the number of times the document was edited.
+        Arguments:
+
+            cycles -- int
+        """
         if type(cycles) is not int:
             raise TypeError, u"cycles must be an int"
         if cycles < 1:
@@ -688,6 +861,13 @@ class odf_document(object):
 
 
     def get_generator(self):
+        """Get the signature of the software that generated this document.
+        Return: unicode
+
+        Example::
+
+
+        """
         meta = self.__get_xmlpart('meta')
         element = meta.get_element('//meta:generator')
         generator = element.get_text()
@@ -695,6 +875,15 @@ class odf_document(object):
 
 
     def set_generator(self, generator):
+        """Set the signature of the software that generated this document.
+        Arguments:
+
+            generator -- unicode
+
+        Example::
+
+            document.set_generator(u"lpOD Project")
+        """
         _check_arguments(text=generator)
         meta = self.__get_xmlpart('meta')
         element = meta.get_element('//meta:generator')
@@ -703,6 +892,20 @@ class odf_document(object):
 
 
     def get_statistic(self):
+        """Get the statistic from the software that generated this document.
+        Return: dict
+
+        Example::
+
+            >>> document.get_statistic():
+            {'meta:table-count': 1,
+             'meta:image-count': 2,
+             'meta:object-count': 3,
+             'meta:page-count': 4,
+             'meta:paragraph-count': 5,
+             'meta:word-count': 6,
+             'meta:character-count': 7}
+        """
         meta = self.__get_xmlpart('meta')
         element = meta.get_element('//meta:document-statistic')
         statistic = {}
@@ -712,6 +915,23 @@ class odf_document(object):
 
 
     def set_statistic(self, statistic):
+        """Set the statistic for the documents: number of words, paragraphs,
+        etc.
+        Arguments:
+
+            statistic -- dict
+
+        Example::
+
+            >>> statistic = {'meta:table-count': 1,
+                             'meta:image-count': 2,
+                             'meta:object-count': 3,
+                             'meta:page-count': 4,
+                             'meta:paragraph-count': 5,
+                             'meta:word-count': 6,
+                             'meta:character-count': 7}
+            >>> document.set_statistic(statistic)
+        """
         if type(statistic) is not dict:
             raise TypeError, "statistic must be a dict"
         meta = self.__get_xmlpart('meta')
@@ -732,6 +952,11 @@ class odf_document(object):
 def odf_get_document(uri):
     """Return an "odf_document" instance of the ODF document stored at the
     given URI.
+
+    Example::
+
+        >>> uri = 'uri://of/a/document.odt'
+        >>> document = odf_get_document(uri)
     """
     container = odf_get_container(uri)
     return odf_document(container)
@@ -740,6 +965,11 @@ def odf_get_document(uri):
 
 def odf_new_document_from_template(template_uri):
     """Return an "odf_document" instance using the given template.
+
+    Example::
+
+        >>> uri = 'uri://of/a/template.ott'
+        >>> document = odf_new_document_from_template(uri)
     """
     container = odf_new_container_from_template(template_uri)
     return odf_document(container)
@@ -748,6 +978,13 @@ def odf_new_document_from_template(template_uri):
 
 def odf_new_document_from_class(odf_class):
     """Return an "odf_document" instance of the given class.
+    Arguments:
+
+        odf_class -- 'text', 'spreadsheet', 'presentation' or 'drawing'
+
+    Example::
+
+        >>> document = odf_new_document_from_class('spreadsheet')
     """
     container = odf_new_container_from_class(odf_class)
     return odf_document(container)
