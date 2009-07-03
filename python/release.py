ccd4ffd625eb3235b27da298aaaa37dd3b25e5e7
David Versmisse <david.versmisse@itaapy.com>
2009-07-03 16:36:34 +0200

    python: use directly distutils instead of itools.pkg

---
+++ python/release.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Copyright (C) 2009 Itaapy, ArsAperta, Pierlis, Talend
+
+# Import from the Standard Library
+from subprocess import Popen, PIPE
+from sys import stderr, argv
+from datetime import datetime
+from os.path import basename
+
+
+
+def _run_command(command):
+    popen = Popen(command, stdout=PIPE, stderr=PIPE)
+    stdoutdata, stderrdata = popen.communicate()
+    if popen.returncode != 0 or stderrdata:
+        raise ValueError
+    return stdoutdata
+
+
+
+def get_date():
+    date = _run_command(['git', 'log', '--pretty=format:%at', '-n1'])
+    return datetime.fromtimestamp(int(date))
+
+
+
+def get_branch():
+    branches = _run_command(['git', 'branch'])
+    for line in branches.splitlines():
+        if line.startswith('*'):
+            return line[2:].rstrip()
+    else:
+        raise ValueError
+
+
+
+def get_git_files():
+    files = _run_command(['git', 'ls-files'])
+    return [ name.strip() for name in files.splitlines() ]
+
+
+
+def get_release():
+    date = get_date()
+    date = date.strftime('%Y%m%d-%H%M')
+    return '%s-%s' % (get_branch(), date)
+
+
+
+if __name__ == '__main__':
+    try:
+        print get_release()
+    except:
+        print>>stderr, '%s: error: unable to read info' % basename(argv[0])
+
+
--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,33 @@
 # -*- coding: UTF-8 -*-
 # Copyright (C) 2009 Itaapy, ArsAperta, Pierlis, Talend
 
-# Import from itools
-from itools.pkg import setup
+# Import from the Standard Library
+from distutils import core
 
-setup()
+# Import from lpod
+from release import get_release, get_git_files
+
+
+
+# Make the version.txt file
+release = get_release()
+open('version.txt', 'w').write(release)
+
+# Make the MANIFEST file and search for the data
+filenames = get_git_files()
+filenames.remove('.gitignore')
+filenames.extend(['MANIFEST', 'version.txt'])
+filenames = [ name for name in filenames if not name.startswith('test') ]
+open('MANIFEST', 'w').write('\n'.join(filenames))
+data_files = [ name for name in filenames if not name.endswith('.py') ]
+
+# And call core.setup ....
+core.setup(description = 'lpOD Library',
+           license = 'GNU Lesser General Public License (LGPL)',
+           name = 'lpod',
+           package_data = {'lpod': data_files},
+           package_dir = {'lpod': ''},
+           packages = ['lpod'],
+           url = 'http://www.lpod-project.org/',
+           version = release)
+
