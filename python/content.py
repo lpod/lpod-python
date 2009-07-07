15f08f4fa9a20992c3bde2d977a6c421a86fb4c7
Hervé Cauwelier <herve@itaapy.com>
2009-07-07 16:44:27 +0200

    insert content element and update API

--- python/content.py
+++ python/content.py
@@ -4,7 +4,7 @@
 # Import from the Standard Library
 
 # Import from lpod
-from utils import _check_arguments, _generate_xpath_query
+from utils import _check_arguments, _make_xpath_query
 from utils import _check_position_or_name, _get_cell_coordinates
 from xmlpart import odf_element, odf_xmlpart, LAST_CHILD
 
@@ -15,17 +15,62 @@ class odf_content(odf_xmlpart):
     body_xpath = '//office:text'
 
 
+    def insert_element(self, element, context, xmlposition=LAST_CHILD,
+                       offset=0):
+        _check_arguments(element=element, context=context,
+                         xmlposition=xmlposition, offset=offset)
+        qname = element.get_name()
+        if qname in ('text:section', 'text:p', 'text:h', 'draw:frame',
+                     'table:table', 'text:list'):
+            # TODO check validity of the context
+            context.insert_element(element, xmlposition)
+        elif qname in ('text:note', 'office:annotation', 'text:span'):
+            if context.get_name() != 'text:p':
+                raise ValueError, "context must be a paragraph"
+            text = context.get_text()
+            before, after = text[:offset], text[offset:]
+            context.set_text(before)
+            element.set_text(after, after=True)
+            context.insert_element(element, xmlposition=LAST_CHILD)
+        elif qname == 'text:list-item':
+            if context.get_name() != 'text:list':
+                raise ValueError, "context must be a list"
+            context.insert_element(element, xmlposition)
+        elif qname == 'draw:image':
+            if context.get_name() != 'draw:frame':
+                raise ValueError, "context must be a frame"
+            context.insert_element(element, xmlposition)
+        elif qname == 'style:style':
+            # XXX we'll probably have add_style/remove_style/...
+            if context.get_name() != 'office:automatic-styles':
+                raise ValueError, "context must be the styles container"
+            context.insert_element(element, xmlposition)
+        # From now on report explicit errors
+        elif qname in ('table:table-cell', 'table:table-row',
+                       'table:table-column'):
+            context.insert_element(element, xmlposition)
+            # TODO raise ValueError, "use the odf_table API"
+        elif qname.startswith('style:') and qname.endswith('-properties'):
+            if context.get_name() != 'style:style':
+                raise ValueError, "context must be a style"
+            context.insert_element(element, xmlposition)
+            # TODO raise ValueError, "use the style API"
+        else:
+            raise ValueError, 'element "%s" is not (yet) supported' % qname
+
+
     #
     # Sections
     #
 
     def get_section_list(self, style=None, context=None):
-        return self.__get_element_list('text:section', style=style,
-                                       context=context)
+        return self._get_element_list('text:section', style=style,
+                                      context=context)
 
 
     def get_section(self, position, context=None):
-        return self.__get_element('text:section', position, context=context)
+        return self._get_element('text:section', position=position,
+                                 context=context)
 
 
     #
@@ -33,12 +78,12 @@ class odf_content(odf_xmlpart):
     #
 
     def get_paragraph_list(self, style=None, context=None):
-        return self.__get_element_list('text:p', style=style,
-                                       context=context)
+        return self._get_element_list('text:p', style=style, context=context)
 
 
     def get_paragraph(self, position, context=None):
-        return self.__get_element('text:p', position, context=context)
+        return self._get_element('text:p', position=position,
+                                 context=context)
 
 
     #
@@ -46,12 +91,13 @@ class odf_content(odf_xmlpart):
     #
 
     def get_span_list(self, style=None, context=None):
-        return self.__get_element_list('text:span', style=style,
-                                       context=context)
+        return self._get_element_list('text:span', style=style,
+                                      context=context)
 
 
     def get_span(self, position, context=None):
-        return self.__get_element('text:span', position, context=context)
+        return self._get_element('text:span', position=position,
+                                 context=context)
 
 
     #
@@ -59,24 +105,12 @@ class odf_content(odf_xmlpart):
     #
 
     def get_heading_list(self, style=None, level=None, context=None):
-        if level is not None:
-            _check_arguments(level=level)
-            attributes = {'text:outline-level': level}
-        else:
-            attributes = None
-        return self.__get_element_list('text:h', style=style,
-                                       attributes=attributes,
-                                       context=context)
+        return self._get_element_list('text:h', style=style, context=context)
 
 
     def get_heading(self, position, level=None, context=None):
-        if level is not None:
-            _check_arguments(level=level)
-            attributes = {'text:outline-level': level}
-        else:
-            attributes = None
-        return self.__get_element('text:h', position, attributes=attributes,
-                                  context=context)
+        return self._get_element('text:h', position=position,
+                                 context=context)
 
 
     #
@@ -84,35 +118,29 @@ class odf_content(odf_xmlpart):
     #
 
     def get_frame_list(self, style=None, context=None):
-        return self.__get_element_list('draw:frame', frame_style=style,
-                                       context=context)
+        return self._get_element_list('draw:frame', style=style,
+                                      context=context)
 
 
-    def get_frame(self, position=None, name=None, context=None):
+    def get_frame(self, name=None, position=None, context=None):
         _check_position_or_name(position, name)
-        attributes = {'draw:name': name} if name is not None else {}
-        return self.__get_element('draw:frame', position,
-                                  attributes=attributes,
-                                  context=context)
+        return self._get_element('draw:frame', frame_name=name,
+                                 position=position, context=context)
 
 
     #
     # Images
     #
 
-    def __insert_image(self, element, context, xmlposition, offset):
-        # XXX If context is None
-        #     => auto create a frame with the good dimensions
-        if context is None:
-            raise NotImplementedError
-
-        self.__insert_element(element, context, xmlposition, offset)
+    def get_image_list(self, style=None, context=None):
+        return self._get_element_list('draw:image', style=style,
+                                      context=context)
 
 
-    def get_image(self, position=None, name=None, context=None):
-        # Automatically get the frame
-        frame = self.get_frame(position, name, context)
-        return self.__get_element('draw:image', context=frame)
+    def get_image(self, name=None, position=None, context=None):
+        _check_position_or_name(position, name)
+        return self._get_element('draw:image', frame_name=name,
+                                 position=position, context=context)
 
 
     #
@@ -120,20 +148,23 @@ class odf_content(odf_xmlpart):
     #
 
     def get_table_list(self, style=None, context=None):
-        return self.__get_element_list('table:table', style=style,
-                                       context=context)
+        return self._get_element_list('table:table', style=style,
+                                      context=context)
 
 
-    def get_table(self, position=None, name=None, context=None):
+    def get_table(self, name=None, position=None, context=None):
         _check_position_or_name(position, name)
-        attributes = {'table:name': name} if name is not None else {}
-        return self.__get_element('table:table', position,
-                                  attributes=attributes, context=context)
+        return self._get_element('draw:image', table_name=name,
+                                 position=position, context=context)
 
+
+    #
+    # Rows
+    #
 
     def get_row_list(self, style=None, context=None):
-        return self.__get_element_list('table:table-row', style=style,
-                                       context=context)
+        return self._get_element_list('table:table-row', style=style,
+                                      context=context)
 
 
     #
@@ -141,8 +172,8 @@ class odf_content(odf_xmlpart):
     #
 
     def get_cell_list(self, style=None, context=None):
-        return self.__get_element_list('table:table-cell', style=style,
-                                       context=context)
+        return self._get_element_list('table:table-cell', style=style,
+                                      context=context)
 
 
     # Warning: This function gives just a "read only" odf_element
@@ -180,19 +211,13 @@ class odf_content(odf_xmlpart):
     #
 
     def get_note_list(self, note_class=None, context=None):
-        _check_arguments(note_class=note_class)
-        if note_class is not None:
-            attributes = {'text:note-class': note_class}
-        else:
-            attributes = None
-        return self.__get_element_list('text:note', attributes=attributes,
-                                       context=context)
+        return self._get_element_list('text:note', note_class=note_class,
+                                      context=context)
 
 
-    def get_note(self, id, context=None):
-        attributes = {'text:id': id}
-        return self.__get_element('text:note', attributes=attributes,
-                                  context=context)
+    def get_note(self, text_id, context=None):
+        return self._get_element('text:note', text_id=text_id,
+                                 context=context)
 
 
     def insert_note_body(self, element, context):
@@ -211,8 +236,8 @@ class odf_content(odf_xmlpart):
         _check_arguments(creator=creator, start_date=start_date,
                          end_date=end_date)
         annotations = []
-        for annotation in self.__get_element_list('office:annotation',
-                                                  context=context):
+        for annotation in self._get_element_list('office:annotation',
+                                                 context=context):
             if creator is not None and creator != annotation.get_creator():
                 continue
             date = annotation.get_date()
@@ -227,10 +252,12 @@ class odf_content(odf_xmlpart):
     def get_annotation(self, creator=None, start_date=None, end_date=None,
                        context=None):
         annotations = self.get_annotation_list(creator=creator,
-                start_date=start_date, end_date=end_date, context=context)
-        if not annotations:
-            return None
-        return annotations[0]
+                                               start_date=start_date,
+                                               end_date=end_date,
+                                               context=context)
+        if annotations:
+            return annotations[0]
+        return None
 
 
     #
@@ -247,27 +274,16 @@ class odf_content(odf_xmlpart):
 
 
     def get_style_list(self, family=None, category=None):
-        _check_arguments(family=family)
-        attributes = {}
-        if family is not None:
-            attributes['style:family'] = family
-        query = _generate_xpath_query('style:style', attributes=attributes)
-        context = self.get_category_context(category)
-        if context is None:
-            return self.get_element_list(query)
-        return context.get_element_list(query)
+        return self._get_element_list('style:style', family=family)
 
 
     def get_style(self, name_or_element, family):
-        _check_arguments(family=family)
         if isinstance(name_or_element, odf_element):
             if not name_or_element.is_style():
                 raise ValueError, "element is not a style element"
+            return name_or_element
         elif type(name_or_element) is str:
-            attributes = {'style:name': name_or_element,
-                          'style:family': family}
-            query = _generate_xpath_query('style:style',
-                                          attributes=attributes)
-            context = self.get_element('//office:automatic-styles')
-            return context.get_element(query)
+            context = self.get_category_context('automatic')
+            return self._get_element('style:style', style=name_or_element,
+                                     family=family)
         raise TypeError, "style name or element expected"
