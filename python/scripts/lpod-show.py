ee763193d02796b0c337bc609c01493659a3225e
David Versmisse <david.versmisse@itaapy.com>
2009-07-23 20:24:59 +0200

    python: lpod-show: The first lines, ...

--- python/scripts/lpod-show.py
+++ python/scripts/lpod-show.py
@@ -2,6 +2,34 @@
 # -*- coding: UTF-8 -*-
 # Copyright (C) 2009 Itaapy, ArsAperta, Pierlis, Talend
 
+# Import from the standard library
+from optparse import OptionParser
+from sys import exit
 
-print 'Hello world!'
+# Import from lpod
+from lpod import __version__
+
+
+
+if  __name__ == '__main__':
+
+    # Options initialisation
+    usage = '%prog <file>'
+    description = 'Extract informations from an OpenDocument file.'
+    parser = OptionParser(usage, version=__version__, description=description)
+
+    # Parse !
+    opts, args = parser.parse_args()
+
+    # Container
+    if len(args) != 1:
+        parser.print_help()
+        exit(1)
+    container = args[0]
+
+    print 'file:', container
+
+
+
+
 
