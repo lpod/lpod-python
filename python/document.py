4561c2d7822f78c61edd94d8d3c9cc8b20f1848e
David Versmisse <david.versmisse@itaapy.com>
2009-07-30 19:00:05 +0200

    python: document: Add odf_create_link

--- python/document.py
+++ python/document.py
@@ -472,6 +472,7 @@ def odf_create_page_number_variable(select_page=None, page_adjust=None):
     """page_adjust is an integer to add (or subtract) to the page number
 
     select_page -- string in ('previous', 'current', 'next')
+
     page_adjust -- int
     """
     element = odf_create_element('<text:page-number/>')
@@ -638,6 +639,44 @@ def odf_create_draw_page(name, master_page=None, page_layout=None, id=None,
                               page_layout)
     if id:
         element.set_attribute('draw:id', id)
+    return element
+
+
+
+def odf_create_link(href, name=None, target_frame=None, style=None,
+                    visited_style=None):
+    """Return a text:a odf_element.
+
+    Arguments:
+
+        href -- string (an URI)
+
+        name -- unicode
+
+        target_name -- '_self', '_blank', '_parent', '_top'
+
+        style -- string
+
+        visited_style -- string
+    """
+    element = odf_create_element('<text:a xlink:href="%s"/>' % href)
+
+    if name is not None:
+        element.set_attribute('office:name', name.encode('utf-8'))
+
+    if target_frame is not None:
+        element.set_attribute('office:target-frame-name', target_frame)
+        if target_frame == '_blank':
+            element.set_attribute('xlink:show', 'new')
+        else:
+            element.set_attribute('xlink:show', 'replace')
+
+    if style is not None:
+        element.set_attribute('text:style-name', style)
+
+    if visited_style is not None:
+        element.set_attribute('text:visited-style-name', visited_style)
+
     return element
 
 
