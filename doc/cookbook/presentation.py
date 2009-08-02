837ebc0381bd3acdb18d9ac1c030895509491700
Luis Belmar-Letelier <luis@itaapy.com>
2009-08-02 17:43:42 +0200

    odp cookbook

--- doc/cookbook/presentation.py
+++ doc/cookbook/presentation.py
@@ -14,64 +14,35 @@ body = document.get_body()
 # DrawPage 1
 page = odf_create_drawpage('page1')
 
-
-# Add a frame with a draw_text-box 
-heading = odf_create_heading(1, text=u'Headings, Paragraph, Liste and Notes')
-body.append_element(heading)
-text-element = odf_create_paragraph(text = u'First SlideShow')
-
-draw_text-box1 = odf_create_text-box(text-elment, height, width=, x=, y=, image_style=)
-
-draw_text-box1 = odf_create_text-box(text-elment, frame=frame)
-
-draw_text-box1 = odf_create_text-box(text-elment, 
-                                     frame_size=('5cm', '100mm'), 
-                                     frame_position=('1cm', '0cm'),
-                                     image_style=xxx)
-
-
+# Add a frame with a draw_text-box
+text-element = odf_create_heading(1, text=u'First Slide')
+draw_text-box = odf_create_frame-text(text-elment,
+                                      frame_size=('5cm', '100mm'),
+                                      frame_position=('1cm', '0cm'),
+                                      style=xxx)
 page.append_element(draw_text-box)
 
-# fill draw_text-box with a paragraph
-text-element = odf_create_list([u'thé', u'café'])
-text-frame = odf_create_text_frame(text-elment, height, width=, x=, y=)
-page.append_element(text-frame)
-
-
-#existe en mémoir n'a pas été sérialisé
-image = odf_create_image(image_uri="toto.jpg", image_style=xxx, link=yes)
-
-draw_image = odf_create_image_frame(image_uri="toto.jpg", 
-                                    frame_size=('5cm', '100mm'), 
+# Add an image frame
+draw_image = odf_create_frame-image(image_uri="toto.jpg",
+                                    link=yes,
+                                    frame_size=('5cm', '100mm'),
                                     frame_position=('1cm', '0cm'),
-                                    image_style=xxx,
-                                    link=yes)
+                                    image_style=xxx)
+page.append_element(draw_image)
+
+# Add the page to the body
+body.append_element(page)
+
+
+# Get a new page, page2 copy of page1
+page2 = page.clone('page2')
+#page2 = body.get_clone('page2', page)
+#page2 = body.get_copy('page2', page)
+
+frame = page2.get_frame()
 
 
 
-
-
-draw_image = odf_create_image(uri, height, width=, x=, y=, image_style=)
-page.append(draw_image)
-
-# introspection
-page.get_frames()
-
-# DrawPage 2
-page = body.()
-
-# Add Heading
-heading = odf_create_heading(level=1, u'Headings, Paragraph, Liste and Notes')
-body.append_element(heading)
-
-# Add Paragraph 
-paragraph = odf_create_paragraph(text = u'lpOD generated Document')
-body.append_element(paragraph)
-
-# A list
-my_list = odf_create_list([u'thé', u'café'])
-item = odf_create_list_item(u'chocolat')
-my_list.append_element(item)
 
 # Save
 document.save('presentation.odp', pretty=True)
