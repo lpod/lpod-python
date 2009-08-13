1b5469b2ca0f6972426fe34a2f57346ff1c5941e
Romain Gauthier <romain@itaapy.com>
2009-08-13 15:57:04 +0200

    python: Continue to reorganize some tests after the big migration.

--- python/test/test_content.py
+++ python/test/test_content.py
@@ -13,31 +13,6 @@ class ContentTestCase(TestCase):
     pass
 
 
-class GetElementTestCase(TestCase):
-
-    def setUp(self):
-        self.document = document = odf_get_document('samples/basetext.odt')
-        self.content = document.get_xmlpart('content')
-
-
-    def tearDown(self):
-        del self.content
-        del self.document
-
-
-    def test_get_element_missed(self):
-        content = self.content
-        paragraph = content.get_paragraph_by_position(999)
-        self.assertEqual(paragraph, None)
-
-
-    def test_get_element_list(self):
-        content = self.content
-        regex = ur'(first|second|a) paragraph'
-        paragraphs = content._get_element_list('//text:p', regex=regex)
-        self.assertEqual(len(paragraphs), 4)
-
-
 
 if __name__ == '__main__':
     main()
--- python/test/test_paragraph.py
+++ python/test/test_paragraph.py
@@ -50,6 +50,23 @@ class TestParagraph(TestCase):
         self.assertEqual(text, "First paragraph of the second section.")
 
 
+    def test_get_paragraph_by_content(self):
+        content = self.content
+        regex = ur'(first|second|a) paragraph'
+        paragraph = content.get_paragraph_by_content(regex)
+        text = paragraph.get_text()
+        self.assertEqual(text, u'This is the first paragraph.')
+
+
+    def test_get_paragraph_by_content_context(self):
+        content = self.content
+        section2 = content.get_section_by_position(2)
+        regex = ur'([Ff]irst|second|a) paragraph'
+        paragraph = content.get_paragraph_by_content(regex, context=section2)
+        text = paragraph.get_text()
+        self.assertEqual(text, u'First paragraph of the second section.')
+
+
     def test_odf_paragraph(self):
         content = self.content
         paragraph = content.get_paragraph_by_position(1)
@@ -73,6 +90,12 @@ class TestParagraph(TestCase):
         body.insert_element(paragraph, LAST_CHILD)
         last_paragraph = clone.get_paragraph_list()[-1]
         self.assertEqual(last_paragraph.get_text(), u'An inserted test')
+
+
+    def test_get_paragraph_missed(self):
+        content = self.content
+        paragraph = content.get_paragraph_by_position(999)
+        self.assertEqual(paragraph, None)
 
 
 
