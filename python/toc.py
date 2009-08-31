fdace1a1e574b8aa79ef802ecc3472c798657f11
Hervé Cauwelier <herve@itaapy.com>
2009-08-31 15:31:24 +0200

    python: create the minimal toc structure that just works

--- python/toc.py
+++ python/toc.py
@@ -2,7 +2,24 @@
 # Copyright (C) 2009 Itaapy, ArsAperta, Pierlis, Talend
 
 # Import from lpod
-from element import register_element_class, odf_element
+from element import register_element_class, odf_create_element, odf_element
+from element import FIRST_CHILD
+from utils import Boolean
+
+
+def odf_create_toc(name=u"Table of content", protected=True, style=None,
+                   outline_level=10):
+    data = '<text:table-of-content/>'
+    element = odf_create_element(data)
+    if name:
+        element.set_attribute('text:name', name)
+    element.set_attribute('text:protected', Boolean.encode(protected))
+    if style:
+        element.set_attribute('text:style-name', style)
+    # A TOC is quite a complex hierarchy
+    element.set_toc_outline_level(outline_level)
+    return element
+
 
 
 class odf_toc(odf_element):
@@ -23,6 +40,21 @@ class odf_toc(odf_element):
                 result.append(element.get_formated_text(context))
         result.append('\n')
         return u''.join(result)
+
+
+    def get_toc_outline_level(self):
+        source = self.get_element('text:table-of-content-source')
+        if source is None:
+            return None
+        return source.get_attribute('text:outline-level')
+
+
+    def set_toc_outline_level(self, level):
+        source = self.get_element('text:table-of-content-source')
+        if source is None:
+            source = odf_create_element('<text:table-of-content-source/>')
+            self.insert_element(source, FIRST_CHILD)
+        source.set_attribute('text:outline-level', level)
 
 
 
