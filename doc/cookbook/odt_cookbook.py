9602d07e533e89c73c39eb33036d3e4641d0dcf8
Hervé Cauwelier <herve@itaapy.com>
2009-09-30 17:43:39 +0200

    doc: complement odt cookbook with notes and table

--- doc/cookbook/odt_cookbook.py
+++ doc/cookbook/odt_cookbook.py
@@ -5,8 +5,9 @@
 from lpod.document import odf_new_document_from_type
 from lpod.heading import odf_create_heading
 from lpod.list import odf_create_list, odf_create_list_item
-from lpod.note import odf_create_note
+from lpod.note import odf_create_note, odf_create_annotation
 from lpod.paragraph import odf_create_paragraph
+from lpod.table import odf_create_table
 from lpod.toc import odf_create_toc
 
 # Creation of the document
@@ -23,11 +24,11 @@ toc = odf_create_toc()
 body.append_element(toc)
 
 # Add Heading
-heading = odf_create_heading(1, text=u'Headings, Paragraphs, Lists and Notes')
+heading = odf_create_heading(1, text=u'Lists')
 body.append_element(heading)
 
 # Add Paragraph
-paragraph = odf_create_paragraph(text=u'lpOD generated Document')
+paragraph = odf_create_paragraph(u'lpOD generated Document')
 body.append_element(paragraph)
 
 #
@@ -40,26 +41,44 @@ item.append_element(odf_create_list([u'thé vert', u'thé rouge']))
 my_list.append_item(item)
 
 # insert item by position
-my_list.insert_item(u'Chicoré', position=1)
+my_list.insert_item(u'Chicorée', position=1)
 
-# insert item by relativ position
+# insert item by relative position
 the = my_list.get_item_by_content(u'thé')
-my_list.insert_item(u'Chicoré', before=the)
-my_list.insert_item(u'Chicoré', after=the)
+my_list.insert_item(u'Chicorée', before=the)
+my_list.insert_item(u'Chicorée', after=the)
 
 body.append_element(my_list)
 
 #
 # Footnote with odf_create_note of class "footnote" and insert_note
 #
-paragraph = odf_create_paragraph(text=u'A paragraph with a footnote '
+body.append_element(odf_create_heading(1, u"Footnotes"))
+paragraph = odf_create_paragraph(u'A paragraph with a footnote '
                                       u'about references in it.')
 note = odf_create_note(note_id='note1', citation=u"1",
                        body=u'Author, A. (2007). "How to cite references", '
                             u'New York: McGraw-Hill.')
 paragraph.insert_note(note, after=u"graph")
-
 body.append_element(paragraph)
+
+#
+# Annotations
+#
+body.append_element(odf_create_heading(1, u"Annotations"))
+paragraph = odf_create_paragraph(u"A paragraph with an annotation "
+                                 u"in the middle.")
+annotation = odf_create_annotation(u"It's so easy!", creator=u"Luis")
+paragraph.insert_annotation(annotation, after=u"annotation")
+body.append_element(paragraph)
+
+#
+# Tables
+#
+body.append_element(odf_create_heading(1, u"Tables"))
+body.append_element(odf_create_paragraph(u"A table:"))
+table = odf_create_table(u"Table 1", width=3, height=3)
+body.append_element(table)
 
 # Save
 document.save('text.odt', pretty=True)
