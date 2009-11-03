0946f28ecf996f567ac06ae54843e77744be0288
Hervé Cauwelier <herve@itaapy.com>
2009-11-03 15:03:31 +0100

    script to highlight patterns in text

--- python/scripts/lpod-style.py
+++ python/scripts/lpod-style.py
@@ -9,6 +9,8 @@ from sys import exit, stdout
 # Import from lpod
 from lpod import __version__
 from lpod.document import odf_get_document
+from lpod.style import odf_create_style
+from lpod.styles import rgb2hex
 
 
 translations = {
@@ -81,6 +83,7 @@ style_families = ['paragraph', 'text',  'graphic', 'page-layout',
 def show(odf_file_url, families):
     """Show the different types of styles of a document and their properties.
     """
+    # Ensure output is consistent across several runs (diff, etc.)
     families.sort()
     document = odf_get_document(odf_file_url)
     output = document.show_styles(family=families)
@@ -93,12 +96,34 @@ def show(odf_file_url, families):
 
 
 
-def merge(source_url, dest_url):
-    source = odf_get_document(source_url)
-    dest = odf_get_document(dest_url)
+def merge(odf_file_url, from_file, pretty=False):
+    source = odf_get_document(from_file)
+    dest = odf_get_document(odf_file_url)
     dest.merge_styles_from(source)
-    dest.save(pretty=True)
+    dest.save(pretty=pretty)
     print "Done (0 error, 0 warning)."
+
+
+
+def highlight(odf_file_url, pattern, color, pretty=False):
+    document = odf_get_document(odf_file_url)
+    display_name = u"Highlight %s" % color
+    print "display_name", display_name
+    name = display_name.replace(u" ", u"_20_")
+    print "name", name
+    # Is our style already installed?
+    style = document.get_style('text', name)
+    if style is None:
+        style = odf_create_style('text', name, display_name,
+                background_color=rgb2hex(color))
+        document.insert_style(style)
+    body = document.get_body()
+    i = -1
+    for i, paragraph in enumerate(body.get_paragraph_list(regex=pattern)
+            + body.get_heading_list(regex=pattern)):
+        paragraph.set_span(name, regex=pattern)
+    document.save(pretty=pretty)
+    print (i + 1), "paragraphs changed (0 error, 0 warning)."
 
 
 
@@ -108,39 +133,56 @@ if  __name__ == '__main__':
     usage = '%prog <file>'
     description = 'A command line interface to manipulate styles of ' \
                   'OpenDocument files.'
-    oparser = OptionParser(usage, version=__version__,
-                           description=description)
+    parser = OptionParser(usage, version=__version__,
+            description=description)
     # --show
     help = ('show the styles of a document. The value is a comma separated '
             'list. Authorized values are automatic, default, master, named '
             'and all.')
-    oparser.add_option('-s', '--show', dest='show', action='store',
-                       default='', metavar='<style list>', help=help)
+    parser.add_option('-s', '--show', dest='show', action='store',
+            default='', metavar='<style list>', help=help)
     # --merge
-    help = ('Copy styles from FILE to <file>. Any style with the same name '
+    help = ('copy styles from FILE to <file>. Any style with the same name '
             'will be replaced.')
-    oparser.add_option('-m', '--merge-styles-from', dest='merge',
-                       action='store', metavar='FILE', help=help)
+    parser.add_option('-m', '--merge-styles-from', dest='merge',
+            action='store', metavar='FILE', help=help)
+    # --highlight
+    help = ("highlight the text matching the regular expression PATTERN in "
+            "the text (IN PLACE)")
+    parser.add_option('-l', '--highlight', dest='highlight', action='store',
+            metavar='PATTERN', help=help)
+    # --color
+    help = ("the name or #rrggbb color of the highlight: black, blue, "
+            "brown, cyan, green, grey, magenta, orange, pink, red, violet, "
+            "white, yellow (default)")
+    parser.add_option('-c', '--color', dest='color', action='store',
+            default='yellow', metavar='COLOR', help=help)
+    # --pretty
+    parser.add_option('-p', '--pretty', dest='pretty', action='store_true',
+                      help="pretty print the XML of the output document")
     # Parse options
-    opts, args = oparser.parse_args()
+    options, args = parser.parse_args()
     if len(args) != 1:
-        oparser.print_help()
+        parser.print_help()
         exit(1)
     odf_file_url = args[0]
 
-    if opts.show:
-        if opts.show in ('', 'all'):
+    if options.show:
+        if options.show in ('', 'all'):
             families = style_families
         else:
             # Transform the comma separated list into a true list object
-            families = [family.strip() for family in opts.show.split(',')]
+            families = [family.strip() for family in options.show.split(',')]
         # Check if the style family is known
         for family in families:
             if family not in style_families:
-                oparser.error('Unknown %s family' % family)
+                parser.error('Unknown %s family' % family)
         show(odf_file_url, families)
-    elif opts.merge:
-        merge(opts.merge, odf_file_url)
+    elif options.merge:
+        merge(odf_file_url, options.merge, pretty=options.pretty)
+    elif options.highlight:
+        highlight(odf_file_url, options.highlight, options.color,
+                pretty=options.pretty)
     else:
-        oparser.print_help()
+        parser.print_help()
         exit(1)
