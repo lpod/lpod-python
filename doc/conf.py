7b0200bf0de2498223b22fe4be83e5631e370571
Hervé Cauwelier <herve@itaapy.com>
2009-11-26 16:06:20 +0100

    fix last-minute errors

--- doc/conf.py
+++ doc/conf.py
@@ -39,8 +39,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'LPOD'
-copyright = u'2009, LPOD team'
+project = u'lpOD'
+copyright = u'2009, lpOD team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
