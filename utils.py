d099b25200f5d86aa3ebeb64c9895eceb62821f6
Hervé Cauwelier <herve@itaapy.com>
2010-02-25 11:33:37 +0100

    remove useless argument repetitions

--- utils.py
+++ utils.py
@@ -234,20 +234,9 @@ def _merge_dicts(d, *args, **kw):
 # Non-public yet useful helpers
 #
 
-def _get_element_list(context, element_name, family=None, text_style=None,
-        draw_name=None, draw_style=None, draw_text_style=None,
-        table_name=None, table_style=None, note_class=None, style_name=None,
-        text_id=None, text_name=None, office_name=None, office_title=None,
-        outline_level=None, level=None, href=None, svg_title=None,
-        svg_desc=None, page_layout=None, position=None, regex=None):
-    query = _make_xpath_query(element_name, family=family,
-            text_style=text_style, draw_name=draw_name,
-            draw_style=draw_style, draw_text_style=draw_text_style,
-            table_name=table_name, table_style=table_style,
-            style_name=style_name, note_class=note_class, text_id=text_id,
-            text_name=text_name, office_name=office_name,
-            office_title=office_title, outline_level=outline_level,
-            level=level, page_layout=page_layout, position=position)
+def _get_element_list(context, element_name, regex=None, href=None,
+        svg_title=None, svg_desc=None, **kw):
+    query = _make_xpath_query(element_name, **kw)
     elements = context.get_element_list(query)
     # Filter the elements with the regex
     if regex is not None:
@@ -260,9 +249,11 @@ def _get_element_list(context, element_name, family=None, text_style=None,
             if search(href, href_attr) is not None:
                 filtered.append(element)
         elements = filtered
-    for variable, childname in [(svg_title, 'svg:title'),
-                                (svg_desc, 'svg:desc')]:
-        if variable:
+    if svg_title is not None or svg_desc is not None:
+        for variable, childname in [(svg_title, 'svg:title'),
+                                    (svg_desc, 'svg:desc')]:
+            if not variable:
+                continue
             filtered = []
             for element in elements:
                 child = element.get_element(childname)
@@ -272,19 +263,8 @@ def _get_element_list(context, element_name, family=None, text_style=None,
     return elements
 
 
-def _get_element(context, element_name, family=None, text_style=None,
-        draw_name=None, table_name=None, table_style=None, style_name=None,
-        text_id=None, text_name=None, office_name=None, office_title=None,
-        outline_level=None, level=None, href=None, svg_title=None,
-        svg_desc=None, page_layout=None, position=None, regex=None):
-    result = _get_element_list(context, element_name, family=family,
-            text_style=text_style, draw_name=draw_name,
-            table_name=table_name, table_style=table_style,
-            style_name=style_name, text_id=text_id, text_name=text_name,
-            office_name=office_name, office_title=office_title,
-            outline_level=outline_level, level=level, href=href,
-            svg_title=svg_title, svg_desc=svg_desc, page_layout=page_layout,
-            position=position, regex=regex)
+def _get_element(context, element_name, **kw):
+    result = _get_element_list(context, element_name, **kw)
     if result:
         return result[0]
     return None
